599c334dd28434b82014a3b3c13adc2d629c1231
Willian Arcênio <willianarcenio@hotmail.com>
2021-10-12 13:45:42 -0300

    Ajuste no Frontend da página de Login.

--- index.php
+++ index.php
@@ -13,26 +13,29 @@
     <link rel="stylesheet" href="skins/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
     <!-- Latest compiled and minified JavaScript -->
     <script src="js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="skins/css/estilo.css">
     <title>Login | Intelligent Ticket</title>
 </head>
-<body>
+<body class="login-page">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <form action="login_DB.php" method="POST">
+            <div class="col-md-12 login-box">
+                <form action="login_DB.php" method="POST" class="login-form">
                     <label for="login" class="label-login">Login:</label>
-                    <input type="text" name="login" id="login" size="50" required="required">
+                    <input type="text" name="login" id="login" size="50" required="required" class="campo">
 
                     <br>
                     <br>
                     
                     <label for="senha" class="label-senha">Senha:</label>
-                    <input type="password" name="senha" id="senha" size="40" required="required">
+                    <input type="password" name="senha" id="senha" size="40" required="required" class="campo">
 
                     <br>
                     <br>
 
-                    <button type="submit" name="enviar">Enviar</button>
+                    <div class="botao-div">
+                        <button type="submit" name="enviar" class="botao">Enviar</button>
+                    </div>
                 </form>
             </div>
         </div>
