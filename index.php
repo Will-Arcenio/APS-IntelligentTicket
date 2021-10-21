222e74803248e2d60dc840e42ce4fac72c180725
Willian Arcênio <willianarcenio@hotmail.com>
2021-10-21 00:02:12 -0300

    Separando o arquivo de estilo do Painel Adm

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="skins/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
     <!-- Latest compiled and minified JavaScript -->
     <script src="js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="skins/css/estilo.css">
+    <link rel="stylesheet" href="skins/css/estilo-admin.css">
     <title>Login | Intelligent Ticket</title>
 </head>
 <body class="login-page-admin adminhtml">
--- view/page/admin/painel_admin.php
+++ view/page/admin/painel_admin.php
@@ -13,9 +13,9 @@
     <link rel="stylesheet" href="../../../skins/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
     <!-- Latest compiled and minified JavaScript -->
     <script src="js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../../skins/css/estilo.css">
+    <link rel="stylesheet" href="../../../skins/css/estilo-admin.css">
     <link rel="stylesheet" href="../../../skins/css/style.css">
-    <title>Painel | Intelligent Ticket</title>
+    <title>Painel | Painel Bla³</title>
 </head>
 <body class="painel-admin">
     <?php 
